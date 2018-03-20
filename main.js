21c9972ca37f25754ff2c5f0463723041b22e5b2
lschachter <lillieschachter@gmail.com>
2018-03-20 16:13:17 -0400

    Add images to replace feed with add image swap between images

--- main.js
+++ main.js
@@ -1,2 +1,57 @@
-let feed = document.getElementById("topnews_main_stream_408239535924329");
+let feed = $("#topnews_main_stream_408239535924329");
+
+let stories = $("#stories_pagelet_rhc");
+let vids = $("#pagelet_video_home_watch_list_rhc");
+let ego = $("#pagelet_ego_pane");
+
+let removals = [stories, vids, ego];
+
+removals.forEach((el) => {
+	el.remove();
+});
+
+let parental = feed.parent();
 feed.remove();
+
+let inspText = $("<h1>You can go for 5 more minutes, I swear!!!</h1>");
+inspText.addClass('beautText');
+parental.append(inspText);
+
+let inspURL = buildImageURL();
+let inspImg = buildImageElement(parental,inspURL);
+
+let pics = [];
+
+for (let i = 0; i < 7; i++) {
+	inspURL = buildImageURL();
+	let img = new Image().src = inspURL;
+	pics.push(img);
+}
+
+setNewImage(pics, inspImg);
+
+function setNewImage(pics, inspImg) {
+	let i = 0;
+	function cycleImages() {
+		$(inspImg).attr('src', pics[i]);
+		i = (i + 1) % pics.length;
+	}
+	setInterval(cycleImages, 10000); 
+};
+
+
+
+function buildImageURL() {
+	let index = Math.floor(Math.random()*23) + 1;
+	let imgURL = chrome.extension.getURL('positive_pics/images-'+index+'.jpg');
+	return imgURL;
+}
+
+function buildImageElement(parental, imgURL) {
+	let inspImg = $("<img src=\""+imgURL+"\">");
+	inspImg.addClass('inspImage');
+	parental.append(inspImg);
+	return inspImg;
+}
+
+ 
